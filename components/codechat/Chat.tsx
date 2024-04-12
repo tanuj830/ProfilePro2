8164decbf36e290f12bfc3041d41571944073f75
tanuj <tanujbhatt644@gmail.com>
2024-04-13 01:06:10 +0530

    hydration error resolved

--- components/codechat/Chat.tsx
+++ components/codechat/Chat.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 import { Input } from "../ui/input";
-import { Button } from "../ui/button";
+import { IoSendOutline } from "react-icons/io5";
 
 const Chat = () => {
   return (
-    <div className=" flex flex-col gap-10 h-full  w-full  bg-primary   rounded-lg overflow-y-scroll hideScrollbar">
-      {/* input chat  */}
-      <div className="h-[20] bg-primary ">
-        <div className="flex items-center gap-2  w-full">
-          <Input className="bg-white w-full" />
-          <Button variant={"secondary"}>Send</Button>
-        </div>
-      </div>
-
-      <div className="h-full">
-        <h6 className="text-sm font-medium">Chat Box</h6>
-        <p className="text-xs text-muted-foreground">
+    <div className=" flex flex-col justify-between gap-1 h-full   w-full shadow-lg bg-muted   rounded-lg overflow-y-scroll hideScrollbar">
+      <div className="h-[90%] overflow-y-scroll p-3 hideScrollbar">
+        <h6 className="text-md font-medium">
+          Chat Box{" "}
+          <span className="text-muted-foreground text-xs">#1290337</span>
+        </h6>
+        <p className="text-xs text-secondary-foreground">
           Chat with your roommates
         </p>
-
-        <div className="flex justify-start items-center mt-2">
+        <div className="flex justify-start items-center mt-5">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hello
             </span>
           </div>
@@ -30,16 +24,16 @@ const Chat = () => {
         <div className="flex justify-end items-center">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hii hdkajhd kjahdjh asjdhak
             </span>
           </div>
         </div>
         {/* ------------------------ */}
-        <div className="flex justify-start items-center mt-2">
+        <div className="flex justify-start items-center mt-5">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hello
             </span>
           </div>
@@ -47,15 +41,15 @@ const Chat = () => {
         <div className="flex justify-end items-center">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hii hdkajhd kjahdjh asjdhak
             </span>
           </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
+        </div>{" "}
+        <div className="flex justify-start items-center mt-5">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hello
             </span>
           </div>
@@ -63,15 +57,15 @@ const Chat = () => {
         <div className="flex justify-end items-center">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hii hdkajhd kjahdjh asjdhak
             </span>
           </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
+        </div>{" "}
+        <div className="flex justify-start items-center mt-5">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hello
             </span>
           </div>
@@ -79,15 +73,15 @@ const Chat = () => {
         <div className="flex justify-end items-center">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hii hdkajhd kjahdjh asjdhak
             </span>
           </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
+        </div>{" "}
+        <div className="flex justify-start items-center mt-5">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hello
             </span>
           </div>
@@ -95,15 +89,15 @@ const Chat = () => {
         <div className="flex justify-end items-center">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hii hdkajhd kjahdjh asjdhak
             </span>
           </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
+        </div>{" "}
+        <div className="flex justify-start items-center mt-5">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hello
             </span>
           </div>
@@ -111,15 +105,15 @@ const Chat = () => {
         <div className="flex justify-end items-center">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hii hdkajhd kjahdjh asjdhak
             </span>
           </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
+        </div>{" "}
+        <div className="flex justify-start items-center mt-5">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hello
             </span>
           </div>
@@ -127,15 +121,15 @@ const Chat = () => {
         <div className="flex justify-end items-center">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hii hdkajhd kjahdjh asjdhak
             </span>
           </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
+        </div>{" "}
+        <div className="flex justify-start items-center mt-5">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hello
             </span>
           </div>
@@ -143,15 +137,15 @@ const Chat = () => {
         <div className="flex justify-end items-center">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hii hdkajhd kjahdjh asjdhak
             </span>
           </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
+        </div>{" "}
+        <div className="flex justify-start items-center mt-5">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hello
             </span>
           </div>
@@ -159,23 +153,15 @@ const Chat = () => {
         <div className="flex justify-end items-center">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hii hdkajhd kjahdjh asjdhak
             </span>
           </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
+        </div>{" "}
+        <div className="flex justify-start items-center mt-5">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
-              Hello
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hello
             </span>
           </div>
@@ -183,108 +169,25 @@ const Chat = () => {
         <div className="flex justify-end items-center">
           <div className="flex flex-col mt-1 ">
             <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
-              Hii hdkajhd kjahdjh asjdhak
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
-              Hello
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-end items-center">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
-              Hii hdkajhd kjahdjh asjdhak
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
-              Hello
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-end items-center">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
-              Hii hdkajhd kjahdjh asjdhak
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
-              Hello
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-end items-center">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
-              Hii hdkajhd kjahdjh asjdhak
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
-              Hello
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-end items-center">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
-              Hii hdkajhd kjahdjh asjdhak
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
-              Hello
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-end items-center">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
-              Hii hdkajhd kjahdjh asjdhak
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-start items-center mt-2">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Anuj</small>
-            <span className="bg-muted/30 px-2 py-1 rounded-lg text-xs">
-              Hello
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-end items-center">
-          <div className="flex flex-col mt-1 ">
-            <small className="text-xs italic">Tanuj</small>
-            <span className="bg-primary-foreground text-muted px-2 py-1 rounded-lg text-xs">
+            <span className="bg-background px-2 py-1 rounded-lg text-xs">
               Hii hdkajhd kjahdjh asjdhak
             </span>
           </div>
         </div>
         {/* --------------------*/}
+      </div>
+      {/* input chat  */}
+      <div className="h-[9%] bg-muted border  rounded-lg overflow-hidden  ">
+        <div className="flex items-center relative  w-full h-full">
+          <input
+            className=" w-full h-full outline-none px-4 text-sm bg-background"
+            placeholder="Hii roommate"
+          />
+          <span className=" rounded-md p-2 absolute right-1 top-1 bottom-1 flex justify-center items-center bg-background cursor-pointer">
+            {/* <SendHorizontalIcon className="w-5 h-7" /> */}
+            <IoSendOutline className="w-5 h-7" />
+          </span>
+        </div>
       </div>
     </div>
   );
--- components/codechat/CodeChatPlayGround.tsx
+++ components/codechat/CodeChatPlayGround.tsx
@@ -6,7 +6,10 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import Chat from "./Chat";
+import { useTheme } from "next-themes";
 const CodeChatPlayGround = () => {
+  const theme = useTheme();
+
   window.onmessage = function (e) {
     if (e.data && e.data.language) {
       console.log(e.data);
@@ -14,21 +17,42 @@ const CodeChatPlayGround = () => {
     }
   };
   return (
-    <div className="h-full">
-      <ResizablePanelGroup direction="horizontal">
-        <ResizablePanel defaultSize={80}>
-          <iframe
-            height="100%"
-            src="https://onecompiler.com/embed/python?codeChangeEvent=true&theme=dark"
-            width="100%"
-          ></iframe>
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel className="px-2 h-full relative" defaultSize={20}>
-          <Chat />
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </div>
+    <>
+      <div className="h-full hidden lg:inline-block w-full">
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel defaultSize={80}>
+            <iframe
+              height="100%"
+              src={`https://onecompiler.com/embed/python?codeChangeEvent=true&${
+                theme.theme === "dark" ? "theme=dark" : null
+              }`}
+              width="100%"
+            ></iframe>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel className="px-5 " defaultSize={20}>
+            <Chat />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+      <div className="h-full inline-block lg:hidden w-full">
+        <ResizablePanelGroup direction="vertical">
+          <ResizablePanel defaultSize={80}>
+            <iframe
+              height="100%"
+              src={`https://onecompiler.com/embed/python?codeChangeEvent=true&${
+                theme.theme === "dark" ? "theme=dark" : null
+              }`}
+              width="100%"
+            ></iframe>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel className="lg:px-5 " defaultSize={20}>
+            <Chat />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+    </>
   );
 };
 
