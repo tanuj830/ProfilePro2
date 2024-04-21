250ceb1b82462e2b4826ba7b642a9b6cb95b4362
tanuj <tanujbhatt644@gmail.com>
2024-04-21 23:24:23 +0530

    changing kinde routes

--- components/resume builder/ConvertToPDF.tsx
+++ components/resume builder/ConvertToPDF.tsx
@@ -1,13 +1,13 @@
-import { usePDF } from "react-to-pdf";
-import Experience from "./experience/Experience";
-const ConvertToPDF = () => {
-  const { toPDF, targetRef } = usePDF({ filename: "page.pdf" });
-  return (
-    <div>
-      <button onClick={() => toPDF()}>Download PDF</button>
-      <div ref={targetRef}>{/* <Experience /> */}</div>
-    </div>
-  );
-};
+// // import { usePDF } from "react-to-pdf";
+// import Experience from "./experience/Experience";
+// // const ConvertToPDF = () => {
+//   const { toPDF, targetRef } = usePDF({ filename: "page.pdf" });
+//   return (
+//     <div>
+//       <button onClick={() => toPDF()}>Download PDF</button>
+//       <div ref={targetRef}>{/* <Experience /> */}</div>
+//     </div>
+//   );
+// };
 
-export default ConvertToPDF;
+// export default ConvertToPDF;
