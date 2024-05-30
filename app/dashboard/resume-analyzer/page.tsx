6c3db8271ad86e5c9f7368ced3b07314350b39d4
tanuj <tanujbhatt644@gmail.com>
2024-05-30 23:31:17 +0530

    code chat ui updated

--- app/dashboard/resume-analyzer/page.tsx
+++ app/dashboard/resume-analyzer/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const ResumeAnalyzer = () => {
   return (
-    <div className="h-[80vh] lg:h-full w-full">
+    <div className="h-[80vh]  w-full">
       <div className="flex justify-center h-full w-full items-center ">
         {/* <h6>Your current route is {path}</h6> */}
         <div className="flex flex-col items-center text-center gap-5">
@@ -14,7 +14,7 @@ const ResumeAnalyzer = () => {
           </span>
           <div className="md:w-[60%]">
             <h5 className="text-foreground font-medium my-4">
-              You dont have any notes created
+              You dont have anything to see
             </h5>
             <p className="text-muted-foreground text-wrap leading-7 text-sm">
               You currently dont have any resume to analyze. Add resume to
