7e8bad244e4a1b54f154e5132857b69ba987269c
tanuj <tanujbhatt644@gmail.com>
2024-06-20 23:05:42 +0530

    commit

--- app/dashboard/code-chat/playground/page.tsx
+++ app/dashboard/code-chat/playground/page.tsx
@@ -165,7 +165,7 @@ const Page = () => {
     };
     setChats((chats) => [...chats, newOutgoingChat]);
     if (message) socket.emit("send_message", { message, username, roomID });
-    setMessage("");
+
   };
 
   const copyRoomId = () => {
